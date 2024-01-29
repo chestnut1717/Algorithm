447e03589facf8159573bb13b0ccad9e0cce0a43
chestnut1717 <62554639+chestnut1717@users.noreply.github.com>
2024-01-30 00:30:45 +0900

    [Silver III] Title: 수열, Time: 208 ms, Memory: 22692 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -1,30 +1,32 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 import java.io.IOException;
+
 public class Main
 {
     static int N;
     static int K;
-    static int[] arr;
     static int[] accSum;
     
 	public static void main(String[] args) throws IOException {
+	    
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] tmp = br.readLine().split("\\s");
-		N = Integer.parseInt(tmp[0]);
-		K = Integer.parseInt(tmp[1]);
+		StringTokenizer st = new StringTokenizer(br.readLine()); // StringTokenizer 활용해서 split
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
 		
-		arr = new int[N];
-		accSum = new int[N+1];
+		accSum = new int[N+1]; // 누적합을 하기 위해 N + 1개의 배열 만듦
 		
-	    String[] tmp2 = br.readLine().split("\\s");
+		st = new StringTokenizer(br.readLine());
 	    for(int i = 0; i < N; i++) {
-	        arr[i] = Integer.parseInt(tmp2[i]);
-	        accSum[i+1] = accSum[i] + arr[i];
+	        accSum[i+1] = accSum[i] + Integer.parseInt(st.nextToken()); // acc의 처음 값은 0이기 때문에, 누적값 가능
 	        
 	    }
 	    
 	    int maximum = -100000000;
+	    
+	    // K개의 연속된 값의 합은, 누적합끼리의 차를 구하는 것과 동일
 	    for(int i = K; i < N + 1; i++) {
 	        maximum = Math.max(maximum, accSum[i] - accSum[i-K]);
 	    }
