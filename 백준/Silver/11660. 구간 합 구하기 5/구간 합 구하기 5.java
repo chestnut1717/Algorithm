4cc3151bf935f45b04511e18ef97fe3ff05d6767
chestnut1717 <62554639+chestnut1717@users.noreply.github.com>
2024-01-31 13:54:35 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1212 ms, Memory: 128208 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+class Main {
+    static int N;
+    static int M;
+    static int[][] arr;
+    static int x1, y1, x2, y2;
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        
+        arr = new int[N+1][N+1];
+
+        // 배열 입력
+        for(int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 1; j <= N; j++) {
+                arr[i][j] = arr[i][j-1] + Integer.parseInt(st.nextToken()); // 행 기준으로 더함
+                
+            }
+        }
+        
+        // x1, y2, x2, y2 입력받기(행, 열)
+        for(int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            x1 = Integer.parseInt(st.nextToken());
+            y1 = Integer.parseInt(st.nextToken());
+            x2 = Integer.parseInt(st.nextToken());
+            y2 = Integer.parseInt(st.nextToken());
+            
+            int height = x2-x1+1;
+            int sum = 0;
+            for(int j = x1; j < x1+height; j++) {
+                sum += arr[j][y2] - arr[j][y1-1];
+            }
+            sb.append(sum).append("\n");
+        }
+        
+        // System.out.println(sb.toString());
+        bw.write(sb.toString());
+        bw.close();
+        
+        
+        
+    }
+}
