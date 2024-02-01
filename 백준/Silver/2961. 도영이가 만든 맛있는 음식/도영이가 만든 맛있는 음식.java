37cb878036deb09a3c3d7fcc5a2d9ea795d2478d
chestnut1717 <62554639+chestnut1717@users.noreply.github.com>
2024-02-01 12:16:50 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 76 ms, Memory: 11500 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
@@ -1,11 +1,13 @@
 import java.util.*;
 import java.io.*;
+
 public class Main {
     static int N;
     static int[] sour;
     static int[] bitter;
     static boolean[] visited;
-    static int taste = Integer.MAX_VALUE;
+    static int taste = Integer.MAX_VALUE; // 최솟값을 구해야 하므로 최댓값을 만들어서 구한다.
+    
 	public static void main(String[] args) throws Exception{
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	    StringTokenizer st;
@@ -21,7 +23,7 @@ public class Main {
 	        sour[i] = Integer.parseInt(st.nextToken());
 	        bitter[i] = Integer.parseInt(st.nextToken());
 	    }
-	    search(0, 1, 0);
+	    search(0, 1, 0, 0);
 	    System.out.println(taste);
 	    
 	    
@@ -29,28 +31,20 @@ public class Main {
 	}
 	
 	// 모든 부분수열 계산하는 코드
-	static void search(int cnt, int tmp_s, int tmp_b) {
+	static void search(int cnt, int tmp_s, int tmp_b, int size) {
 	    // 종료조건
-	    boolean flag = false;
 	    if (cnt == N) {
-	        for(int i = 0; i < N; i++) {
-	            if (visited[i] == true) {
-	                flag = true;
-                    break;
-	            }
-	        }
-	        if (flag) {
+	        if (size > 0) {
 	           int tmp_taste = Math.abs(tmp_s - tmp_b);
-        	   taste = Math.min(taste, tmp_taste);
-        	   
+        	   taste = Math.min(taste, tmp_taste); 
 	        }
 	        return;
 	    }
         
         visited[cnt] = true;
-	    search(cnt+1, tmp_s * sour[cnt], tmp_b + bitter[cnt]);
+	    search(cnt+1, tmp_s * sour[cnt], tmp_b + bitter[cnt], size+1);
 	    visited[cnt] = false;
-	    search(cnt+1, tmp_s, tmp_b);
+	    search(cnt+1, tmp_s, tmp_b, size);
 
 	    
 	}
