5cd8c5ccb5d6cfa0e4278652b365131c5fecd7b6
chestnut1717 <62554639+chestnut1717@users.noreply.github.com>
2024-02-23 15:31:23 +0900

    [Gold IV] Title: ⚾, Time: 632 ms, Memory: 82544 KB -BaekjoonHub

--- 백준/Gold/17281. ⚾/⚾.java
+++ 백준/Gold/17281. ⚾/⚾.java
@@ -1,9 +1,16 @@
 import java.util.*;
 import java.io.*;
 
-		// 1. 가능한 모든 경우의 수를 계산한다.(대신, 1번 선수는 4번 타자로 고정!)
-		// 2. 각 경우수를 직접 play하도록 한다.
-		// 3. 마지막 이닝이 끝나고 나서 점수를 갱신하도록 한다.
+/**
+ * @since jdk1.8
+ * - 문제 : BJ 17281 야구
+ * - 소요시간 : 1시간 30분 
+ * - 난이도 : 상
+ * - 메모리 : 82680 KB / 시간 : 696 ms
+ * - 아이디어 : 빡구현 + 순열 문제
+ * - 선수의 순서를 정하는 전략을 구할 때, 1번타자(해당 코드에서 0)는 무조건 고정시켜놓고 진행해야 시간초과 방지할 수 있음
+ * - 경우의 수 : 9!(모든 선수 순열) x 9(이닝의 수) x 27(한 이닝마다 최대 게임의 수) x 3(각 이닝마다 hit할 경우 ru 배열 정리) = 대략 3,000만
+ */
 public class Main {
 	static int N;
 	static int[][] game;
@@ -16,8 +23,9 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 		StringTokenizer st;
-
-		game = new int[N][9]; // N이닝에 담을 게임을 우선 모두 입력한다.
+        
+        // N이닝에 담을 게임을 우선 모두 입력한다.
+		game = new int[N][9]; 
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < 9; j++) {
@@ -35,17 +43,17 @@ public class Main {
 			    tmpPoint += play(n, strategy);
 			}
 			
-			// 이닝이 끝나면 점수 정산
+			// 3. 이닝이 끝나면 점수 정산
 			maxPoint = Math.max(tmpPoint, maxPoint);
 		}
 		
 		System.out.println(maxPoint);
 
 	}
-	// 이닝과 전략이 주어지면, 해당 전략 플레이
+	// 이닝(n)과 전략(strategy)이 주어지면, 해당 전략 플레이
 	static int play(int n, Queue<Integer> strategy) {
 	    int out = 0; // 아웃 count
-	    int point = 0;
+	    int point = 0; // 현재 이닝에서의 점수
 	    int[] ru = new int[4]; //1루부터
 	    
 	    // 각 플레이어마다 게임 짝지어 주기
@@ -53,18 +61,13 @@ public class Main {
 	        int player = strategy.poll(); // 사전에 타자 정한 순서에서 플레이어 하나씩 꺼내기
 	        strategy.offer(player); // 다시 다음 주자로 넣어준다!
 	        int type = game[n-1][player]; // 해당 플레이어가 공 어떻게 쳤는지(안타, 홈런 등)
+	        
+	        // out일 경우
 	        if (type == 0) {
 	            out++;
 	            continue;
 	        }
-	        else if (type == 4) {
-	            for(int j = 1; j <=3 ; j++) {
-	                point += ru[j];
-	                ru[j] = 0;
-	            }
-	            point += 1; // 자기 자신 점수
-	            continue;
-	        }
+	        
 	        point += hit(type, ru);
 	        
 	        
@@ -72,8 +75,19 @@ public class Main {
 	    return point; // 해당 이닝에 존재한 점수 모두 반환
 	}
 	
+	// 공을 쳤을 때(1~3루타 + 홈런)
 	static int hit(int type, int[] ru) {
 	    int point = 0;
+	    // 홈런일 경우
+	    if (type == 4) {
+            for(int j = 1; j <=3 ; j++) {
+                point += ru[j];
+                ru[j] = 0;
+            }
+            point += 1; // 자기 자신 점수
+            return point;  
+	    }
+	    // 홈런을 제외한 경우 => 1~3루에 있는 사람 count하고 초기화
 	    for(int i = 3; i > 3-type; i--) {
 	        point += ru[i];
 	        ru[i] = 0;
