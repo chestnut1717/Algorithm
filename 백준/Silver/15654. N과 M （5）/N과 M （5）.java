2de584fc7899dadd498667fb3557322c18b0b8e0
chestnut1717 <62554639+chestnut1717@users.noreply.github.com>
2024-02-26 00:06:44 +0900

    [Silver III] Title: N과 M (5), Time: 248 ms, Memory: 30896 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,55 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    static int N;
+    static int M;
+    static int[] input;
+    static int[] numbers;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		input = new int[N];
+		visited = new boolean[N];
+		numbers = new int[M];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+		    input[i] = Integer.parseInt(st.nextToken());
+		}
+
+		
+		Arrays.sort(input);
+		
+		perm(0);
+		bw.write(sb.toString());
+		bw.close();
+	}
+	
+	// 중복조합 구하기
+	static void perm(int cnt){
+	    if(cnt == M) {
+	        for(int i = 0; i < M; i++){
+	            sb.append(numbers[i]).append(" ");
+	        }
+	        sb.append('\n');
+	        return;
+	    }
+	    for(int i = 0; i < N; i++){
+	        if(visited[i]) continue;
+	        visited[i] = true;
+	        numbers[cnt] = input[i];
+	        perm(cnt+1);
+	        visited[i] = false;
+	    }
+	    
+	    
+	}
+}
