ee566113d8829985ba0eda265cfaa18b1363340a
chestnut1717 <62554639+chestnut1717@users.noreply.github.com>
2024-04-07 12:14:53 +0900

    [Gold III] Title: 크게 만들기, Time: 204 ms, Memory: 20544 KB -BaekjoonHub

--- 백준/Gold/2812. 크게 만들기/크게 만들기.java
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.java
@@ -6,7 +6,6 @@ public class Main
 {
     static int N, K;
     static int[] number;
-    static Stack<Integer> stk = new Stack<>();
 	public static void main(String[] args) throws Exception{
 	    
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,31 +19,23 @@ public class Main
 	    // 길이 N의 숫자 입력
 	    number = new int[N];
 	    String tmpStr = br.readLine();
+	    int top = -1; // 스택처럼 활용하기 위해 배열 중 top 가리키는 idx
+	    int cnt = K;
 	    for(int i = 0; i < N; i++) {
-	        number[i] = tmpStr.charAt(i) - '0';
-	    }
-	    
-	    
-	    // 이제 각각의 n을 탐색하면서
-	    for(int num: number) {
-	        // 다음에 들어오는 것이 스택의 값보다 클 때 => 지워저야함
-	        while(K > 0 && !stk.isEmpty() && stk.peek() < num) {
-	            K--;
-	            stk.pop();
+	        int num = tmpStr.charAt(i) - '0';
+	        while(cnt > 0 && top > -1 && number[top] < num) {
+	            number[top] = -1;
+	            top--;
+	            cnt--;
 	        }
-	        stk.push(num);
+	        top++;
+	        number[top] = num;
 	    }
 	    
-	    // K개 남은 것들을 처리해 줘야 함 => 이때는 뒤에것부터 빼줘도 무방
-	    // 항상 비내림차순으로 들어간 게 보장되므로
-	    while(K > 0) {
-	        stk.pop();
-	        K--;
-	    }
-	    
-	    for(int num: stk) {
-	        sb.append(num);
-	    }
+
+        for(int i = 0; i < N-K; i++) {
+            sb.append(number[i]);
+        }
 	    
 	    
 	    
