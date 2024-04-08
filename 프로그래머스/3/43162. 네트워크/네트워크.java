2e413d064ccdd7bd1f84df8a7b7e2964111354a6
chestnut1717 <62554639+chestnut1717@users.noreply.github.com>
2024-04-09 01:28:06 +0900

    [level 3] Title: 네트워크, Time: 0.07 ms, Memory: 71.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,56 +1,24 @@
-import java.util.*;
-import java.io.*;
-// 1:10;
 class Solution {
-    static int N; // 컴퓨터 개수
-    static List<List<Integer>> list = new ArrayList<>(); // 인접리스트 생성
-    static int result = 0;
-    static boolean visited[];
     public int solution(int n, int[][] computers) {
         int answer = 0;
         
-        N = computers.length;
-        // 인접리스트 초기화
-        for(int i = 0; i < N; i++) {
-            list.add(new ArrayList<Integer>());
-        }
+        boolean[] visited = new boolean[n];
         
-        // 그리고 입력을 받음
-        for(int i = 0; i < N; i++) {
-            for(int j = 0; j < N; j++) {
-                if(i == j) continue;
-                if(computers[i][j] == 1) {
-                    list.get(i).add(j);
-                }
-            }
-        }
-        
-        visited = new boolean[N];
-        for(int i = 0; i < N; i++) {
+        for(int i = 0; i < n; i++) {
             if(!visited[i]) {
-                bfs(i);
-                result++;
+                dfs(i, visited, computers);
+                answer++;
             }
         }
         
-        return result;
+        return answer;
     }
-    
-    static void bfs(int num) {
-        // bfs를 통해서 해당 점과 인접한 connected component 찾아냄
-        Queue<Integer> q = new ArrayDeque<>();
-        q.offer(num);
-        visited[num] = true;
-        
-        while(!q.isEmpty()) {
-            int nowNum = q.poll();
-            for(int nextNum: list.get(nowNum)) {
-                if(!visited[nextNum]) {
-                    visited[nextNum] = true;
-                    q.offer(nextNum);
-                }
+    static void dfs(int start, boolean[] visited, int[][] computers) {
+        visited[start] = true;
+        for(int next = 0; next < computers.length; next++) {
+            if(!visited[next] && computers[start][next] == 1) {
+                dfs(next, visited, computers);
             }
         }
-        
     }
 }
