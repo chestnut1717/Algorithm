9d8759a8dde4d351040e0e7b5c229282fe0941d1
chestnut1717 <62554639+chestnut1717@users.noreply.github.com>
2024-04-09 23:44:35 +0900

    [Silver II] Title: N과 M (10), Time: 80 ms, Memory: 11656 KB -BaekjoonHub

--- 백준/Silver/15664. N과 M （10）/N과 M （10）.java
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -4,11 +4,8 @@ import java.io.*;
 public class Main
 {
     static int N, M;
-    // static boolean[] visited = new boolean[10001];
     static int[] arr;
     static int[] num;
-    static boolean[] visited;
-    static HashSet<List<Integer>> set = new HashSet<>();
     
     static StringBuilder sb = new StringBuilder();
 	public static void main(String[] args) throws Exception{
@@ -23,7 +20,6 @@ public class Main
 		// N개의 수 입력
 		arr = new int[N];
 		num = new int[M];
-		visited = new boolean[N];
 		st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < N; i++) {
 		    arr[i] = Integer.parseInt(st.nextToken());
@@ -38,26 +34,20 @@ public class Main
 	
 	static void combi(int depth, int start) {
 	    if(depth == M) {
-	        List<Integer> tmp = new ArrayList<>();
-	        for(int i = 0; i < M; i++) {
-	            tmp.add(num[i]);
+            for(int i = 0; i < M; i++) {
+	            sb.append(num[i]).append(' ');
 	        }
-	        if(!set.contains(tmp)) {
-	            set.add(tmp);
-	            for(int i = 0; i < M; i++) {
-    	            sb.append(num[i]).append(' ');
-    	        }
-    	        sb.append('\n');
-	        }
+	        sb.append('\n');
 
 	        return;
 	    }
+	    int before = -1; // 이전과 같은 값인지 중복 체크해주는 역할
 	    for(int i = start; i < N; i++) {
-	        if(visited[i]) continue;
-	        visited[i] = true;
+	        if(before == arr[i]) continue;
+	        before = arr[i]; // 이전 값과 다르니깐 before 갱신
 	        num[depth] = arr[i];
 	        combi(depth+1, i+1);
-	        visited[i] = false;
+
 	    }
 	}
 }
