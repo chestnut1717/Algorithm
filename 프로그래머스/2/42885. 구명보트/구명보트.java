ee6c5b39db801c37fed80782aa600e5b15010580
chestnut1717 <62554639+chestnut1717@users.noreply.github.com>
2024-04-10 14:47:50 +0900

    [level 2] Title: 구명보트, Time: 9.99 ms, Memory: 53.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int start = 0;
+        int end = people.length-1;
+        int answer = 0;
+        
+        
+        Arrays.sort(people); // O(NlogN)
+        
+        /// two pointer로 풀어야 함, O(N)
+        while(start <= end) {
+            if(people[start] + people[end] <= limit) {
+                start++;
+                end--;
+            } else {
+                end--;
+            }
+            answer++;
+            if(start==end) {
+                answer++;
+                break;
+            }
+        }
+        
+        return answer;
+
+    }
+}  
