ea679bfb2656098023d414df453ce9dc4dfa07f0
chestnut1717 <62554639+chestnut1717@users.noreply.github.com>
2024-04-11 16:45:42 +0900

    [level 2] Title: 모음 사전, Time: 1.53 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -2,7 +2,7 @@
 class Solution {
     static char[] alpha = {'A', 'E', 'I', 'O', 'U'};
     static StringBuilder sb = new StringBuilder();
-    static int cnt = 1;
+    static int cnt = 0;
     static int answer = -1;
     static boolean flag;
     public int solution(String word) {
@@ -13,21 +13,24 @@ class Solution {
     
     // 부분집합 + 중복순열
     static void perm(int depth, String word) {
-        if(flag == true) return;
-        if(depth >= 5) return;
+        
+        if (sb.toString().equals(word)) {
+            answer = cnt;
+            return;
+        }
+        
+        if(depth == 5) {
+            return;
+        }
+        
         for(int i = 0; i < 5; i++) {
             sb.append(alpha[i]);
-            if (flag == false && sb.toString().equals(word)) {
-                flag = true;
-                answer = cnt;
-                return;
-            } else {
-                cnt++;
-                perm(depth+1, word);
-                sb.setLength(sb.length()-1);
-            }
-
+            cnt++;
+            perm(depth+1, word);
+            sb.setLength(sb.length() - 1);
         }
-        return;
+        
     }
+
 }
+
