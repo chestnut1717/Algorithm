7ee79806fca874eeb8df3c8ed6d97d0a41e2d1e0
chestnut1717 <62554639+chestnut1717@users.noreply.github.com>
2024-04-12 22:27:51 +0900

    [Silver III] Title: N과 M (1), Time: 152 ms, Memory: 19888 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -3,8 +3,8 @@ import java.util.*;
 
 public class Main
 {
-    static int N, M; //nPm
-    static int[] number;
+    static int N, M;
+    static int[] numbers;
     static boolean[] visited;
     static StringBuilder sb = new StringBuilder();
 	public static void main(String[] args) throws Exception{
@@ -15,30 +15,30 @@ public class Main
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		
-		number = new int[M];
+		// 배열 초기화
+		numbers = new int[M];
 		visited = new boolean[N+1];
-		permutation(0);
+		perm(0);
 		
 		bw.write(sb.toString());
 		bw.close();
 		
+		
 	}
-	
-	static void permutation(int depth) {
+	static void perm(int depth) {
 	    if(depth == M) {
 	        for(int i = 0; i < M; i++) {
-	            sb.append(number[i]).append(" ");
+	            sb.append(numbers[i]).append(' ');
 	        }
 	        sb.append('\n');
-	        return;
+	       return;
 	    }
-	    
 	    for(int i = 1; i <= N; i++) {
 	        if(visited[i]) continue;
-	        visited[i] = true;
-	        number[depth] = i;
-	        permutation(depth+1);
-	        visited[i] = false;
+            visited[i] = true;
+            numbers[depth] = i;
+            perm(depth+1);
+            visited[i] = false;
 	    }
 	}
 }
