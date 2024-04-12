4a3572d6e288bd07e66f51b896be9c15bafe649f
chestnut1717 <62554639+chestnut1717@users.noreply.github.com>
2024-04-12 22:36:40 +0900

    [Silver III] Title: N과 M (3), Time: 316 ms, Memory: 111720 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,44 +1,42 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main{
-    static int N;
-    static int M;
-    static int[] input;
+public class Main
+{
+    static int N, M;
     static int[] numbers;
+    static boolean[] visited;
     static StringBuilder sb = new StringBuilder();
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		
-		input = new int[N];
+		// 배열 초기화
 		numbers = new int[M];
-		for(int i = 1; i <= N; i++) {
-		    input[i-1] = i;
-		}
-		
 		perm(0);
 		
 		bw.write(sb.toString());
 		bw.close();
+		
+		
 	}
-	
-	// 중복순열 구하기
-	static void perm(int cnt){
-	    if(cnt == M) {
-	        for(int i = 0; i < M; i++){
-	            sb.append(numbers[i]).append(" ");
+
+	// 중복순열
+	static void perm(int depth) {
+	    if(depth == M) {
+	        for(int i = 0; i < M; i++) {
+	            sb.append(numbers[i]).append(' ');
 	        }
 	        sb.append('\n');
 	        return;
 	    }
-	    for(int i = 0; i < N; i++){
-	        numbers[cnt] = input[i];
-	        perm(cnt + 1);
+	    for(int i = 1; i <= N; i++) {
+	        numbers[depth] = i;
+	        perm(depth+1);
 	    }
-	} 
+	}
 }
