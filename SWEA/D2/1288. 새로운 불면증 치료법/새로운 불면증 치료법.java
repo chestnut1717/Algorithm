2e79e03145b6c64e1497374e59aac1ffaf2e9097
chestnut1717 <62554639+chestnut1717@users.noreply.github.com>
2024-06-16 23:36:01 +0900

    [D2] Title: 새로운 불면증 치료법, Time: 179 ms, Memory: 25,624 KB -BaekjoonHub

--- SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.java
+++ SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.java
@@ -9,23 +9,29 @@ class Solution
 		for(int test_case = 1; test_case <= T; test_case++) {
 		    // 우선 입력을 받은 다음, map을 초기화하자.
 		    String num = br.readLine();
-		    int pivot = Integer.parseInt(num);
 		    int N = Integer.parseInt(num);
+		    int cnt = 1; // 배수를 구하기 위한 count
 		    
-		    Set<Character> set = new HashSet<>();
+		    int mainBit = 0;
+		    int comp = (1 << 10) - 1;
 		    
-		    // 이제 각각 구한다.
-		    while(set.size() < 10) {
-		        char[] charArr = Integer.toString(N).toCharArray();
+		    
+		    // mainBit가 1111111111이 나올 때까지
+		    // 그리고 cnt는 최대 45만 되고, N <= 10^6이므로 충분히 int내에서 처리 가능)
+		    while(true){
+		        char[] charArr = Integer.toString(N * cnt).toCharArray();
 		        for(char c: charArr) {
-		            set.add(c);
-		            
+		            int n = c - '0';
+		            mainBit = mainBit | (1 << n);
 		        }
-
-		        if(set.size() == 10) break;
-		        N += pivot;
+		        if((mainBit & comp) == comp) {
+		            break;
+		        }
+		        cnt++;
 		    }
-		    System.out.printf("#%d %d\n",test_case, N);
+		    
+		    
+		    System.out.printf("#%d %d\n",test_case, N * cnt);
 		    
 		}
 	}
