069c2414d86dd28400434241057be1b7a1327158
chestnut1717 <62554639+chestnut1717@users.noreply.github.com>
2024-06-28 00:39:51 +0900

    [Silver III] Title: N과 M (1), Time: 140 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,28 +1,22 @@
 import sys
 input = sys.stdin.readline
 
-def dfs(limit, depth):
-    if depth == limit:
-        for num in ans_list:
-            print(f"{num}", end=' ')
-        print('\n', end='')
+def dfs(depth):
+    if depth == M:
+        print(' '.join(map(str, ans_list)))
         return
     
     for n in range(1, N+1):
-        if isVisited[n] == True:
-            continue
-        isVisited[n] = True
-        ans_list[depth] = n
-        dfs(limit, depth+1)
-        isVisited[n] = False
+        if not isVisited[n]:
+            isVisited[n] = True
+            ans_list.append(n)
+            dfs(depth + 1)
+            isVisited[n] = False
+            ans_list.pop()
 
 N, M = map(int, input().split())
 
-# 1~ N
-numbers = [i for i in range(1, N+1)]
-ans_list = [0 for i in range(M)]
+ans_list = []
+isVisited = [False] * (N + 1)
 
-# N개의 False
-isVisited = [False for _ in range(N+1)]
-
-dfs(M, 0)
+dfs(0)
