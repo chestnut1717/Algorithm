7622f7fe8cb218812d2098dcc78dd99aa7f02f75
chestnut1717 <62554639+chestnut1717@users.noreply.github.com>
2024-07-02 17:39:04 +0900

    [Gold V] Title: 농장 관리, Time: 108 ms, Memory: 13908 KB -BaekjoonHub

--- 백준/Gold/1245. 농장 관리/농장 관리.java
+++ 백준/Gold/1245. 농장 관리/농장 관리.java
@@ -29,7 +29,9 @@ public class Main
 		for(int i = 0; i < N; i++) {
 		    for(int j = 0; j < M; j++) {
 		        if (visited[i][j]) continue;
-
+                
+                // 산봉우리인지 여부
+                boolean isSan = true;
 
 		        // 산봉우리 후보 넣어두는 리스트
 		        List<Pair> list = new ArrayList<>();
@@ -40,8 +42,6 @@ public class Main
 		        Queue<Pair> q = new ArrayDeque<>();
 		        // 우선 넣어주고
 		        q.offer(new Pair(i, j));
-		        list.add(new Pair(i, j));
-		        // 방문처리
 		        visited[i][j] = true;
 		        
 		       	while(!q.isEmpty()) {
@@ -52,56 +52,32 @@ public class Main
 		            for(int a = 0; a < 8; a++) {
 		                int ny = y + dy[a];
 		                int nx = x + dx[a];
-		                // 그리드를 벗어나지 않고, 방문하지 않은거면서 이전 값과 다음 값이 동일한지
-		                if(isValid(ny, nx) && !visited[ny][nx]) {
-		                    visited[ny][nx] = true;
-		                    if(arr[y][x] == arr[ny][nx]) {
-    		                    q.offer(new Pair(ny, nx));
-    		                    list.add(new Pair(ny, nx));
-    		                }
-    		                else if(arr[y][x] < arr[ny][nx]) {
-    		                    visited[ny][nx] = false;
-    		                }
+		                // 그리드를 벗어나지 않고
+		                if(isValid(ny, nx)) {
+		                    // 방문하지 않았고 봉우리가 확장가능한 경우
+		                    if(!visited[ny][nx] && arr[ny][nx] == height) {
+		                        visited[ny][nx] = true;
+		                        q.offer(new Pair(ny, nx));
+		                    }
+		                    
+		                    // 방문 여부와 상관없이 봉우리 탐색 가능
+		                    if(arr[ny][nx] > height) {
+		                        isSan = false;
+		                    }
 		                }
 
 		            }
 		        }
+		        
+		        if(isSan) result++;
 
-		        // 그리고 산봉우리 후보군이 진짜 산봉우리인지 탐색하자.
-		        if(list.size() > 0) {
-                    if(isSan(list)) result++;
-		        }
+
 		    }
 		}
 		
 		System.out.println(result);
 	}
-	static boolean isSan(List<Pair> list) {
-	    boolean sanVisited[][] = new boolean[N][M];
-        Queue<Pair> q = new ArrayDeque<>();
-        for(Pair p: list) {
-            q.offer(p);
-            sanVisited[p.y][p.x] = true;
-        }
-        
-        int height = arr[q.peek().y][q.peek().x];
-        while(!q.isEmpty()) {
-            Pair coor = q.poll();
-            int y = coor.y;
-            int x = coor.x;
-            
-            for(int i = 0; i < 8; i++) {
-                int ny = y + dy[i];
-                int nx = x + dx[i];
-                if(isValid(ny, nx) && !sanVisited[ny][nx]) {
-                    sanVisited[ny][nx] = true;
-                    // 혹시라도 주위의 값 중 하나라도 같거나 크다면 => 나가리
-                    if(height <= arr[ny][nx]) return false;
-                }
-            }
-        }
-        return true;
-	}
+
 	static boolean isValid(int y, int x) {
 	    return 0 <= y && y < N && 0 <= x && x < M;
 	}
@@ -111,10 +87,6 @@ public class Main
 	    public Pair(int y, int x) {
 	        this.y = y;
 	        this.x = x;
-	    }
-	    
-	    public String toString(){
-	        return String.format("(y: %d, x: %d)\n", this.y, this.x);
 	    }
 	}
 }
