020b3a7be01ea0d23f2c8da4004ee22082d4f5f8
chestnut1717 <62554639+chestnut1717@users.noreply.github.com>
2024-07-07 22:23:15 +0900

    [Silver I] Title: 팔, Time: 76 ms, Memory: 11504 KB -BaekjoonHub

--- 백준/Silver/1105. 팔/팔.java
+++ 백준/Silver/1105. 팔/팔.java
@@ -1,34 +1,38 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.*;
+import java.io.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException{
+public class Main
+{
+    static char[] L, R;
+	public static void main(String[] args) throws Exception{
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		L = st.nextToken().toCharArray();
+		R = st.nextToken().toCharArray();
+		
+		int result = 0;
+		
+		// 일단 배열의 수가 다르면 => 자릿수의 수도 다르다는 의미 => 무조건 0
+		if(L.length != R.length) {
+		    System.out.println(result);
+		// 자릿수가 같을 경우
+		} else {
+            // 모든 자릿수를 탐색해서, 각 자릿수가 모두 8로 끼어있는지 확읺나다.
+    		for(int i = 0; i < L.length; i++) {
+    		    if(L[i] == '8' && R[i] == '8') {
+    		        result++;
+    		    } else if(L[i] == R[i]) {
+    		        continue;
+    		    }
+    		    else{
+    		        break;
+    		    }
+    		}
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s = br.readLine();
+    		System.out.println(result);
+		}
 
-        String[] arr = s.split(" ");
-
-        String a = arr[0];
-        String b = arr[1];
-
-        if (a.length() != b.length()) {
-            System.out.println(0);
-            return;
-        }
-
-        int count = 0;
-        for (int i = 0; i < a.length(); i++) {
-            if(a.charAt(i) == '8' && b.charAt(i) == '8'){
-                count ++;
-            }else if(a.charAt(i) == b.charAt(i)){
-                continue;
-            }else{
-                break;
-            }
-        }
-
-        System.out.println(count);
-    }
+		
+		
+	}
 }
