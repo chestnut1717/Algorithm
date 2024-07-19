25d30e79108b458f6aac865e7f37a46acc96e6af
chestnut1717 <62554639+chestnut1717@users.noreply.github.com>
2024-07-20 02:37:58 +0900

    [Gold V] Title: 신촌 통폐합 계획, Time: 656 ms, Memory: 161780 KB -BaekjoonHub

--- 백준/Gold/31423. 신촌 통폐합 계획/신촌 통폐합 계획.java
+++ 백준/Gold/31423. 신촌 통폐합 계획/신촌 통폐합 계획.java
@@ -1,74 +1,53 @@
 import java.util.*;
 import java.io.*;
 
-public class Main
-{
+public class Main {
     static int N;
     static String[] schools;
-    static List<CustomLinkedList> list = new ArrayList<>();
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-		N = Integer.parseInt(br.readLine());
-		
-		// 학교 이름
-		schools = new String[N+1];
-		
-		for(int i = 0; i <= N; i++) {
-		    list.add(new CustomLinkedList(new Node(i)));
-		}
-		for(int i = 1; i <= N; i++) {
-		    schools[i] = br.readLine();
-		}
-		
-		// N-1개 입력 받기
-		int last = 0;
-		for(int i = 0; i < N-1; i++) {
-		    st = new StringTokenizer(br.readLine());
-		    int addSchool = Integer.parseInt(st.nextToken());
-		    int removeSchool = Integer.parseInt(st.nextToken());
-		    
-		    list.get(addSchool).addLast(list.get(removeSchool));
-		    // 마지막 숫자
-		    if (i == N-2) {
-		        last = addSchool;
-		    }
-		}
-		
-		Node now = list.get(last).head;
-		while(now != null) {
-		    sb.append(schools[now.num]);
-		    now = now.next;
+    static int[] next;
+    static int[] tail;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        N = Integer.parseInt(br.readLine());
+
+        // 학교 이름
+        schools = new String[N + 1];
+        next = new int[N + 1];
+        tail = new int[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            schools[i] = br.readLine();
+            next[i] = i;
+			tail[i] = i;
+        }
+
+        // N-1개 입력 받기
+        int cur = -1;
+        for (int i = 0; i < N - 1; i++) {
+            st = new StringTokenizer(br.readLine());
+            int addSchool = Integer.parseInt(st.nextToken());
+            int removeSchool = Integer.parseInt(st.nextToken());
+
+            // 연결을 갱신
+            next[tail[addSchool]] = removeSchool;
+            tail[addSchool] = tail[removeSchool];
+            cur = addSchool;
+        }
+
+
+        // 연결 리스트 순회하며 문자열 구성
+        for (int i = 0; i < N; i++) {
+			sb.append(schools[cur]);
+			cur = next[cur];
 		}
 
-		bw.write(sb.toString());
-		bw.close();
-	}
-	
-	static class Node {
-	    int num;
-	    Node next;
-	    public Node(int num) {
-	        this.num = num;
-	        this.next = null;
-	    }
-	}
-	
-	static class CustomLinkedList{
-	    Node head;
-	    Node tail;
-	    
-	    public CustomLinkedList(Node node) {
-	        this.head = node;
-	        this.tail = node;
-	    }
-	    
-	    public void addLast(CustomLinkedList otherList) {
-	        this.tail.next = otherList.head;
-	        this.tail = otherList.tail;
-	    }
-	    
-	}
+        bw.write(sb.toString());
+        bw.close();
+    }
+
+
 }
