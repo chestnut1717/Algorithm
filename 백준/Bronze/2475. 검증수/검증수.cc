dd6db1d88343551b0d29f882f2ea050d1e180be1
chestnut1717 <62554639+chestnut1717@users.noreply.github.com>
2024-07-22 00:35:58 +0900

    [Bronze V] Title: 검증수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2475. 검증수/검증수.cc
+++ 백준/Bronze/2475. 검증수/검증수.cc
@@ -1,24 +1,21 @@
 #include <iostream>
 #include <cmath>
-#define N 5
 
 using namespace std;
 
-int arr[N];
+
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
+
     
-    
-    for(int i = 0; i < N; i++) {
-        cin >> arr[i];
-    }
-    
+    int tmp;
     int num = 0;
-    for(int i = 0; i < N; i++) {
-        num += pow(arr[i], 2);
+    for(int i = 0; i < 5; i++) {
+        cin >> tmp;
+        num += pow(tmp, 2);
     }
     
     cout << num % 10;
